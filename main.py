b6b04988fd5996d2375749da0520dac16baaea03
DevHyung <khuphj@gmail.com>
2018-07-25 16:38:24 +0900

    Complete 엑셀기능

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 if __name__=="__main__":
     #===CONFIG
-    baseUrl = 'https://band.us/'
+    wb = Workbook()
+    ws1 = wb.worksheets[0]
+    header1 = ['이름', 'URL', 'member_count']
+    ws1.column_dimensions['A'].width = 30
+    ws1.column_dimensions['B'].width = 50
+    ws1.column_dimensions['C'].width = 20
+    ws1.append(header1)
+
     keyword = input(">>> 키워드를 입력하세요 :: ")
+    min = int( input(">>> Min ~ MAX 멤버수중 Min값을 입력하세요(포함) :") )
+    max = int(input(">>> {} ~ MAX 멤버수중 Min값을 입력하세요(포함) :".format(min)))
+    print(">>> {} ~ {} 멤버밴드만 추출을 시작합니다.".format(min,max))
+    baseUrl = 'https://band.us/'
     url = 'https://band.us/discover/search/'+keyword
     driver = webdriver.Chrome('./chromedriver.exe')
     driver.maximize_window()
@@ -17,43 +28,27 @@ if __name__=="__main__":
     driver.find_element_by_xpath('//*[@id="input_search_view90"]').send_keys(keyword+Keys.ENTER)
     time.sleep(2)
     cnt = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/section/div/h1/span').text.strip()
-    print(cnt)
+    print(">>> 총 검색 갯수 : ",cnt)
     lisLength = 0
     while int(cnt) > lisLength:
         body = driver.find_element_by_css_selector('body')
         bs4 = BeautifulSoup(driver.page_source,'lxml')
         lis = bs4.find('ul',class_='cCoverList _bandListContainer').find_all('li')
         lisLength = len(lis)
-        print(lisLength)
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         #body.send_keys(Keys.PAGE_DOWN)
         time.sleep(0.3)
     # 모두 페이지소스에 박혔었을떄
+    print(">>> 검색완료 추출 시작")
     for li in lis:
         try:
             bandUrl = baseUrl + li.find('a')['href']
             name = li.find('strong',class_='name').find('span').get_text().strip()
             memberCnt = int ( li.find('strong',class_='totalNumber').get_text().strip() )
-            if memberCnt > 20:
-                print(name,memberCnt,bandUrl)
+            if min <= memberCnt <= max:
+                ws1.append([name,bandUrl,memberCnt])
         except: # 맨마지막 경우
             pass
-
-
-    """
-    header1 = ['이름', 'URL', 'member_count']
-    wb = Workbook()
-    ws1 = wb.worksheets[0]
-    ws1.column_dimensions['A'].width = 30
-    ws1.column_dimensions['B'].width = 50
-    ws1.column_dimensions['C'].width = 20
-    ws1.append(header1)
-    # 데이터 삽입
-    # itemlist 가 [품명,최저가,링크] 이런식으로 온걸
-    # openpyxl 객체 ws1 에 append 시키면 들어감
-    for itemlist in datalist:
-        ws1.append(itemlist)
+    print(">>> 추출완료 파일저장중")
     wb.save(keyword + ".xlsx")
-    print(">>> 파일저장완료")
-    input("엔터키나 아무키를 누르시면 종료됩니다. ")
-    """
+
